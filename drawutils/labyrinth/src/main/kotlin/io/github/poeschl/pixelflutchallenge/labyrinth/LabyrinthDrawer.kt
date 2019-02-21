99295bae764e2e6b2f5627e7cb116f842bb8a714
Markus Pöschl <Poeschl@users.noreply.github.com>
2019-02-21 20:23:00 +0100

    Working labyrinth

--- drawutils/labyrinth/src/main/kotlin/io/github/poeschl/pixelflutchallenge/labyrinth/LabyrinthDrawer.kt
+++ drawutils/labyrinth/src/main/kotlin/io/github/poeschl/pixelflutchallenge/labyrinth/LabyrinthDrawer.kt
@@ -1,5 +1,65 @@
 package io.github.poeschl.pixelflutchallenge.labyrinth
 
-fun main() {
-    println("Drawing nothing")
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import de.amr.graph.grid.impl.OrthogonalGrid
+import de.amr.maze.alg.traversal.RandomBFS
+import io.github.poeschl.pixelflutchallenge.shared.PixelFlutInterface
+import io.github.poeschl.pixelflutchallenge.shared.Point
+import io.github.poeschl.pixelflutchallenge.shared.drawRect
+import java.awt.Color
+
+fun main(args: Array<String>) {
+    ArgParser(args).parseInto(::Args).run {
+        println("Start drawing on $host:$port")
+        LabyrinthDrawer(host, port).start()
+    }
+}
+
+class LabyrinthDrawer(private val host: String, private val port: Int) {
+
+    companion object {
+        private const val SPLIT_COUNT = 3
+        private val MAZE_CELL = Point(1, 1)
+        private val MAZE_SIZE = Pair(10, 10)
+    }
+
+    private val drawInterface = PixelFlutInterface(host, port)
+
+    private val displaySize = drawInterface.getPlaygroundSize()
+
+    fun start() {
+        println("Detected size $displaySize")
+
+        val mazeOrigin = Point(
+            (displaySize.first / SPLIT_COUNT) * MAZE_CELL.x,
+            (displaySize.second / SPLIT_COUNT) * MAZE_CELL.y
+        )
+        println("Maze Origin: $mazeOrigin")
+
+        drawRect(drawInterface, mazeOrigin, Pair(displaySize.first / SPLIT_COUNT, displaySize.second / SPLIT_COUNT), Color.BLACK)
+
+        val mazeGrid = createNewMazeGrid(Point(0, 0), MAZE_SIZE)
+        val mazeDrawer = Maze(mazeOrigin, MAZE_SIZE)
+
+        mazeGrid.edges().forEach { println("${it.either()} -> ${it.other()}") }
+        mazeDrawer.updateMaze(mazeGrid.edges())
+
+        println("Draw maze")
+        mazeDrawer.draw(drawInterface)
+
+        drawInterface.close()
+    }
+
+    private fun createNewMazeGrid(start: Point, size: Pair<Int, Int>): OrthogonalGrid {
+        val mazeGen = RandomBFS(size.first, size.second)
+        mazeGen.createMaze(start.x, start.y)
+        return mazeGen.grid
+    }
+
+}
+
+class Args(parser: ArgParser) {
+    val host by parser.storing("--host", help = "The host of the pixelflut server").default("localhost")
+    val port by parser.storing("-p", "--port", help = "The port of the server") { toInt() }.default(1234)
 }
---
+++ drawutils/labyrinth/src/main/kotlin/io/github/poeschl/pixelflutchallenge/labyrinth/Maze.kt
@@ -0,0 +1,82 @@
+package io.github.poeschl.pixelflutchallenge.labyrinth
+
+import de.amr.graph.core.api.Edge
+import io.github.poeschl.pixelflutchallenge.shared.*
+import java.awt.Color
+import java.util.stream.Stream
+
+class Maze(private val origin: Point, private val mazeCellSize: Pair<Int, Int>) {
+
+    companion object {
+        private const val DEBUG = false
+        private const val PATH_SIZE = 8
+        private const val BORDER_WIDTH = 1
+        private const val CELL_SIZE = PATH_SIZE + BORDER_WIDTH * 2
+        private val WALL_COLOR = Color.WHITE
+    }
+
+    private var shadowMaze = mutableSetOf<Pixel>()
+    private var mazeSet = mutableSetOf<Pixel>()
+
+    fun updateMaze(edges: Stream<Edge>) {
+        for (x: Int in 0 until mazeCellSize.first) {
+            for (y: Int in 0 until mazeCellSize.second) {
+                createVertexes(Point(x, y))
+            }
+        }
+        edges.forEach { drawEdge(it) }
+        mazeSet = shadowMaze.filter { it.color != Color.BLACK }.toHashSet()
+    }
+
+    fun draw(drawInterface: PixelFlutInterface) {
+        drawInterface.paintPixelSet(mazeSet)
+    }
+
+    private fun createVertexes(position: Point) {
+
+        val vertexOrigin = origin.plus(
+            Point(position.x * CELL_SIZE, position.y * CELL_SIZE)
+        )
+        shadowMaze.addAll(createHorizontalPixels(vertexOrigin, CELL_SIZE, WALL_COLOR))
+        shadowMaze.addAll(createHorizontalPixels(vertexOrigin.plus(Point(0, CELL_SIZE)), CELL_SIZE, WALL_COLOR))
+        shadowMaze.addAll(createVerticalPixels(vertexOrigin, CELL_SIZE, WALL_COLOR))
+        shadowMaze.addAll(createVerticalPixels(vertexOrigin.plus(Point(CELL_SIZE, 0)), CELL_SIZE, WALL_COLOR))
+        shadowMaze.add(Pixel(vertexOrigin.plus(Point(CELL_SIZE, CELL_SIZE)), WALL_COLOR))
+    }
+
+    private fun drawEdge(edge: Edge) {
+        val from = getPointOfMazeCell(Math.min(edge.either(), edge.other()))
+        val to = getPointOfMazeCell(Math.max(edge.either(), edge.other()))
+
+        when {
+            from.x == to.x && from.y != to.y -> drawVerticalPath(from, to)
+            from.y == to.y && from.x != to.x -> removeVerticalBorderToRightOf(from)
+        }
+    }
+
+    private fun removeVerticalBorderToRightOf(from: Point) {
+        for (yOffset in 0..PATH_SIZE) {
+            val wallPoint = from.plus(Point(CELL_SIZE, BORDER_WIDTH + yOffset))
+            shadowMaze.removeIf { it.point == origin.plus(wallPoint) }
+            if (DEBUG) {
+                shadowMaze.add(Pixel(origin.plus(wallPoint), Color.RED))
+            }
+        }
+    }
+
+    private fun drawVerticalPath(from: Point, to: Point) {
+        for (xOffset in 0..PATH_SIZE) {
+            val wallPoint = from.plus(Point(BORDER_WIDTH + xOffset, CELL_SIZE))
+            shadowMaze.removeIf { it.point == origin.plus(wallPoint) }
+            if (DEBUG) {
+                shadowMaze.add(Pixel(origin.plus(wallPoint), Color.RED))
+            }
+        }
+    }
+
+    private fun getPointOfMazeCell(index: Int): Point {
+        val y = (index / mazeCellSize.first) * CELL_SIZE
+        val x = (index % mazeCellSize.second) * CELL_SIZE
+        return Point(x, y)
+    }
+}
